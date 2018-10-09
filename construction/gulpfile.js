87e544a6f59e04c428a0654a22ed7bce730e686f
artem <artem010191.mail.ru>
2018-10-09 21:06:20 +0300

    [not completed] construction

--- construction/gulpfile.js
+++ construction/gulpfile.js
@@ -1,2 +1,8 @@
-var gulp = require('gulp');
+let gulp = require('gulp'),
+    handlebars = require('gulp-handlebars');
 
+gulp.task('build', function() {
+    gulp.src('handlebars/*.handlebars')
+        .pipe(handlebars())
+        .pipe(gulp.dest('../TEST'))
+});
